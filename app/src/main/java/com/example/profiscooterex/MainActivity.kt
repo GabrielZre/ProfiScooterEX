1554e53605be41de29347418d1f7d9e778d75d62
GabrielZre <104325153+GabrielZre@users.noreply.github.com>
2023-05-01 13:06:52 +0200

    first version

---
+++ app/src/main/java/com/example/profiscooterex/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.profiscooterex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.profiscooterex.presentation.Navigation
+import com.example.profiscooterex.presentation.login.LoginViewModel
+import com.example.profiscooterex.ui.theme.ProfiScooterEXTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val loginViewModel = viewModel(modelClass = LoginViewModel::class.java)
+            ProfiScooterEXTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation(loginViewModel = loginViewModel)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/profiscooterex/presentation/home/Home.kt
@@ -0,0 +1,9 @@
+package com.example.profiscooterex.presentation.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Home() {
+    Text(text = "Welcome")
+}
